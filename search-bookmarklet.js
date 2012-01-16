393f981295f70cf7287c1d5f62d87f5c1b63e8cd
Eric Phetteplace <phette23@gmail.com>
2012-01-15 19:18:16 -0500

    throw an alert if body isn't loaded yet

--- search-bookmarklet.js
+++ search-bookmarklet.js
@@ -1,14 +1,21 @@
 // inspired by & based on a bookmarklet by Barbara Arnett and Valerie Forrestal at the
 // Stevens Institute of Technology, 2011. See https://www.stevens.edu/library/research/
 (function (window, document, undefined) {
-    var a=document.title.replace(' - Wikipedia, the free encyclopedia','')
-		.replace(' - Google Search','')
-		.replace(' - Bing','')
-		.replace(' - Yahoo! Search Results','');
-    void(a=prompt('Search the Chesapeake College Library for:',a));
-    if(a)
-        {
-        var b='http://catalog.chesapeake.edu/view.aspx?keyword='+a;
-        window.location.href=b;
+    var d=document;
+    try {
+	if(!d.body) throw(0);
+	var a=d.title.replace(' - Wikipedia, the free encyclopedia','')
+		    .replace(' - Google Search','')
+		    .replace(' - Bing','')
+		    .replace(' - Yahoo! Search Results','');
+	void(a=prompt('Search the Chesapeake College Library for:',a));
+	if(a)
+	    {
+	    var b='http://catalog.chesapeake.edu/view.aspx?keyword='+a;
+	    window.location.href=b;
+	    }
+	}
+    catch(e) {
+        alert('Please wait until the page is finished loading.');
         }
 }(window, document));
