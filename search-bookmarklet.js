e61ce445778affc923f6e49a3f66a13c485b04ba
Eric Phetteplace <phette23@gmail.com>
2012-06-21 22:09:48 -0400

    minor, mainly stylistic, tweaks

--- search-bookmarklet.js
+++ search-bookmarklet.js
@@ -3,6 +3,7 @@
 (function ( window, document, undefined ) {
     var d = document;
     try {
+<<<<<<< HEAD
     if ( !d.body ) throw(0);
     var a = d.title.replace( ' - Wikipedia, the free encyclopedia', '' )
             .replace( ' - Google Search', '' )
@@ -15,9 +16,26 @@
     if (a) {
         var b = 'http://catalog.chesapeake.edu/view.aspx?keyword=' + a;
         window.location.href = b;
+=======
+        if ( !d.body ) throw(0);
+        var a = d.title.replace( ' - Wikipedia, the free encyclopedia', '' )
+                .replace( ' - Google Search', '' )
+                .replace( ' - Bing', '' )
+                .replace( ' - Yahoo! Search Results', '' )
+                .replace( 'Amazon.com: ', '' )
+                .replace( 'BARNES & NOBLE | ', '' );
+                // NB: add Amazon, other popular websites
+        a = prompt( 'Search the Chesapeake College Library for:', a );
+        if (a) {
+            window.location.href = 'http://catalog.chesapeake.edu/view.aspx?keyword=' + a;
+>>>>>>> minor, mainly stylistic, tweaks
         }
     }
     catch(e) {
         alert( 'Please wait until the page is finished loading.' );
     }
+<<<<<<< HEAD
 } (window, document));
+=======
+} ( window, document ));
+>>>>>>> minor, mainly stylistic, tweaks
