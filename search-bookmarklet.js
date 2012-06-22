83254eadd5e73e0c906bf1d2b1d9e8d15a1c70c7
Eric Phetteplace <phette23@gmail.com>
2012-06-21 22:19:14 -0400

    git is hard

--- search-bookmarklet.js
+++ search-bookmarklet.js
@@ -3,20 +3,6 @@
 (function ( window, document, undefined ) {
     var d = document;
     try {
-<<<<<<< HEAD
-    if ( !d.body ) throw(0);
-    var a = d.title.replace( ' - Wikipedia, the free encyclopedia', '' )
-            .replace( ' - Google Search', '' )
-            .replace( ' - Bing', '' )
-            .replace( ' - Yahoo! Search Results', '' )
-            .replace( 'Amazon.com: ', '')
-            .replace( 'BARNES & NOBLE | ', '');
-            // NB: add Amazon, other popular websites
-    void( a = prompt( 'Search the Chesapeake College Library for:', a ) );
-    if (a) {
-        var b = 'http://catalog.chesapeake.edu/view.aspx?keyword=' + a;
-        window.location.href = b;
-=======
         if ( !d.body ) throw(0);
         var a = d.title.replace( ' - Wikipedia, the free encyclopedia', '' )
                 .replace( ' - Google Search', '' )
@@ -28,14 +14,9 @@
         a = prompt( 'Search the Chesapeake College Library for:', a );
         if (a) {
             window.location.href = 'http://catalog.chesapeake.edu/view.aspx?keyword=' + a;
->>>>>>> minor, mainly stylistic, tweaks
         }
     }
     catch(e) {
         alert( 'Please wait until the page is finished loading.' );
     }
-<<<<<<< HEAD
-} (window, document));
-=======
 } ( window, document ));
->>>>>>> minor, mainly stylistic, tweaks
